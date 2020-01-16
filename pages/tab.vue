c8419e9497b6aeb5017f1ea8bd5eee20f2a77474
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-16 21:23:26 +0800

    搜索框静态样式出现

---
+++ pages/tab.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+      <div>
+          <div class="tab">
+              <span v-for="(item,index) in ['tab1','tab2','tab3']" :key="index" @click='number=index'>{{item}}</span>
+          </div>
+          <div v-if="number == 0">11111111111111111111111111111111111111111111111111111111111111111111</div>
+          <div v-if="number == 1">22222222222222222222222222222222222222222222222222222222222222222222</div>
+          <div v-if="number == 2">33333333333333333333333333333333333333333333333333333333333333333333</div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            number : 0
+        }
+    }
+}
+</script>
+
+<style lang='less' scoped>
+    .tab{
+        cursor: pointer;
+        span{
+            margin: 20px;
+        }
+    }
+</style>
