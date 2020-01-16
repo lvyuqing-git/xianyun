bb8a7bf9d9e4ad19681b050b7df3472d5bb4ebcf
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-16 20:12:56 +0800

    主页轮播图渲染完成

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,5 +5,24 @@
 </template>
 
 <style>
-
+* {
+  margin: 0;
+  padding: 0;
+}
+ul,
+li,
+ol {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+a:hover {
+  color: inherit;
+}
+em,
+i {
+  font-style: normal;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
-      <Header></Header>
+    <Header></Header>
+    <el-carousel height="700px">
+      <el-carousel-item v-for="(item,index) in ['http://157.122.54.189:9095/assets/images/th01.jfif','http://157.122.54.189:9095/assets/images/th02.jfif']"
+                        :key="index">
+        <div class="carousel"  :style="`background: url(${item})`"> 
+           
+        </div>
+      </el-carousel-item>
+    </el-carousel>
+    
   </div>
 </template>
 
 <script>
 import Header from '@/components/header'
 export default {
-    components: {
-        Header
-    }
+  components: {
+    Header
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.carousel{
+   
+    height: 700px;;
+}
 </style>
