f3a2fe4e9b4deee0435cc1066131f65ba5d57adf
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-16 20:14:14 +0800

    底部渲染完成

--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,29 @@
     <el-carousel height="700px">
       <el-carousel-item v-for="(item,index) in ['http://157.122.54.189:9095/assets/images/th01.jfif','http://157.122.54.189:9095/assets/images/th02.jfif']"
                         :key="index">
-        <div class="carousel"  :style="`background: url(${item})`"> 
-           
+        <div class="carousel"
+             :style="`background: url(${item})`">
+
         </div>
       </el-carousel-item>
     </el-carousel>
-    
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from '@/components/header'
+import Footer from '@/components/footer'
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
 
 <style lang='less' scoped>
-.carousel{
-   
-    height: 700px;;
+.carousel {
+  height: 700px;
 }
 </style>
