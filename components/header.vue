53e13d37a2a89f7227440b0e368bb2a1ef6218f9
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-16 17:42:50 +0800

    头部结构搭建

---
+++ components/header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+      <div>
+          <nuxt-link to="/">
+               <img src="http://157.122.54.189:9093/images/logo.jpg" alt="">
+          </nuxt-link>
+      </div>
+      <div>
+          <nuxt-link to="/">首页</nuxt-link>
+          <nuxt-link to="/post">旅游攻略</nuxt-link>
+          <nuxt-link to="/hotel">酒店</nuxt-link>
+          <nuxt-link to="air">国内机票</nuxt-link>
+      </div>
+      <div>
+          <nuxt-link to="/user/login">登录/注册</nuxt-link>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+    
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-      首页
+      <Header></Header>
   </div>
 </template>
 
 <script>
+import Header from '@/components/header'
 export default {
-
+    components: {
+        Header
+    }
 }
 </script>
 
