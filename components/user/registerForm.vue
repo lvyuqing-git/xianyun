00ae3afe2d8ddfb9e5f18e58ad3ed9eaf3b06077
一打砖头拍不倒 <lyq15360562781@163.com>
2020-02-16 22:05:25 +0800

    设置注册列表信息验证

--- components/user/registerForm.vue
+++ components/user/registerForm.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <el-form :model="registerForm"
-             :rules="registerForm"
+             :rules="rules"
              ref="registerForm"
              label-width="70px"
              class="demo-ruleForm">
       <el-form-item label="手机号"
-                    prop="name">
+                    prop="username">
         <el-input v-model="registerForm.username"></el-input>
       </el-form-item>
       <el-form-item label="昵称"
-                    prop="name">
+                    prop="nickname">
         <el-input v-model="registerForm.nickname"></el-input>
       </el-form-item>
       <el-form-item label="验证码"
-                    prop="name">
+                    prop="captcha">
         <el-input v-model="registerForm.captcha"
                   class="captcha">
         </el-input>
@@ -23,14 +23,16 @@
 
       </el-form-item>
       <el-form-item label="密码"
-                    prop="name">
+                    prop="password">
         <el-input v-model="registerForm.password"></el-input>
       </el-form-item>
       <el-form-item label="确认密码"
-                    prop="name">
+                    prop="checkPass">
         <el-input v-model="registerForm.configPassword"></el-input>
       </el-form-item>
-      <el-button type="primary" class="btnRegister">注册</el-button>
+      <el-button type="primary"
+                 class="btnRegister"
+                 @click="register">注册</el-button>
     </el-form>
   </div>
 </template>
@@ -38,6 +40,25 @@
 <script>
 export default {
   data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.registerForm.configPassword !== '') {
+          this.$refs.registerForm.validateField('checkPass')
+        }
+        callback()
+      }
+    }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.registerForm.password) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
     return {
       registerForm: {
         username: '',
@@ -45,7 +66,29 @@ export default {
         captcha: '',
         password: '',
         configPassword: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入手机号', trigger: 'blur' }
+        ],
+        nickname: [{ required: true, message: '请输入昵称', trigger: 'blur' }],
+        captcha: [{ required: true, message: '请输入验证码', trigger: 'blur' }],
+        password: [{ validator: validatePass, trigger: 'blur' }],
+        checkPass: [{ validator: validatePass2, trigger: 'blur' }]
       }
+    }
+  },
+  methods: {
+    register() {
+      this.$axios({
+        url: '/captchas',
+        method: 'POST',
+        data: {
+          tel: this.registerForm.username
+        }
+      }).then(res => {
+        console.log(res)
+      })
     }
   }
 }
@@ -74,8 +117,11 @@ export default {
 .el-button {
   padding: 12px 6px;
 }
-.btnRegister{
-    margin-bottom: 20px;
-    padding: 15px 30px;
+.btnRegister {
+  margin-bottom: 20px;
+  padding: 15px 30px;
+}
+/deep/.el-form-item__error {
+  left: 32px;
 }
 </style>
